80984a26540e6472e72f367eebef82fdbc04509d
Anthon Pang <anthonp@nationalfibre.net>
2014-05-23 15:39:27 +0000

    Re-arrange some code for legibility

--- src/VIPSoft/DoctrineDataFixturesExtension/Service/FixtureService.php
+++ src/VIPSoft/DoctrineDataFixturesExtension/Service/FixtureService.php
@@ -325,17 +325,16 @@ class FixtureService
             return;
         }
 
-        $connection   = $this->entityManager->getConnection();
-        $container    = $this->kernel->getContainer();
-        $outputWriter = new OutputWriter(
-            function () {
-            }
-        );
-
-        $namespace = $container->getParameter('doctrine_migrations.namespace');
+        $connection = $this->entityManager->getConnection();
+        $container  = $this->kernel->getContainer();
+        $namespace  = $container->getParameter('doctrine_migrations.namespace');
 
         if ($namespace) {
-            $directory = $container->getParameter('doctrine_migrations.dir_name');
+            $directory    = $container->getParameter('doctrine_migrations.dir_name');
+            $outputWriter = new OutputWriter(
+                function () {
+                }
+            );
 
             $configuration = new Configuration($connection, $outputWriter);
             $configuration->setMigrationsNamespace($namespace);
