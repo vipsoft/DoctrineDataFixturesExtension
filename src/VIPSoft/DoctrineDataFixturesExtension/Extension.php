9f5cf2e965dcc22fe56d1c1aa02c4ff0fae2aea8
Anthon Pang <anthonp@nationalfibre.net>
2014-05-29 15:26:02 +0000

    Tighten up configuration definition

--- src/VIPSoft/DoctrineDataFixturesExtension/Extension.php
+++ src/VIPSoft/DoctrineDataFixturesExtension/Extension.php
@@ -29,8 +29,7 @@ class Extension implements ExtensionInterface
                 scalarNode('autoload')->
                     defaultValue(true)->
                 end()->
-                arrayNode('migrations')->
-                    prototype('scalar')->end()->
+                variableNode('migrations')->
                 end()->
                 arrayNode('directories')->
                     prototype('scalar')->end()->
@@ -45,7 +44,7 @@ class Extension implements ExtensionInterface
                         thenInvalid('Invalid fixtures lifetime "%s"')->
                     end()->
                 end()->
-                scalarNode('use_backup')->
+                booleanNode('use_backup')->
                     defaultValue(true)->
                 end()->
             end();
@@ -59,28 +58,20 @@ class Extension implements ExtensionInterface
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/services'));
         $loader->load('core.xml');
 
-        if (isset($config['autoload'])) {
-            $container->setParameter('behat.doctrine_data_fixtures.autoload', $config['autoload']);
-        }
-
         if (isset($config['migrations'])) {
-            $container->setParameter('behat.doctrine_data_fixtures.migrations', $config['migrations']);
+            $config['migrations'] = (array) $config['migrations'];
+
+            if ( ! class_exists('Doctrine\DBAL\Migrations\Migration')) {
+               throw new \RuntimeException('Configuration requires doctrine/migrations package');
+            }
         }
 
-        if (isset($config['directories'])) {
-            $container->setParameter('behat.doctrine_data_fixtures.directories', $config['directories']);
-        }
-
-        if (isset($config['fixtures'])) {
-            $container->setParameter('behat.doctrine_data_fixtures.fixtures', $config['fixtures']);
-        }
-
-        $container->setParameter(
-            'behat.doctrine_data_fixtures.use_backup',
-            isset($config['use_backup']) ? $config['use_backup'] : true
-        );
-
+        $container->setParameter('behat.doctrine_data_fixtures.autoload', $config['autoload']);
+        $container->setParameter('behat.doctrine_data_fixtures.directories', $config['directories']);
+        $container->setParameter('behat.doctrine_data_fixtures.fixtures', $config['fixtures']);
         $container->setParameter('behat.doctrine_data_fixtures.lifetime', $config['lifetime']);
+        $container->setParameter('behat.doctrine_data_fixtures.migrations', $config['migrations']);
+        $container->setParameter('behat.doctrine_data_fixtures.use_backup', $config['use_backup']);
     }
 
     /**
